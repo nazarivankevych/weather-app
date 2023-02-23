59ad14ad0cb54adb1946530824e62f421008c538
NazarIvankevych <nazar.ivankevych@gmail.com>
2023-02-23 16:15:34 +0200

    Create a functions and they filter data and insert into display(). Also fix issue with icon.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import tkinter as tk
 from PIL import Image, ImageTk
 from io import BytesIO
+import logging
 
 API = "5cf098f5fb66d2693ac64dd801381bc7"
 
@@ -15,25 +16,48 @@ def get_weather(city):
     return request
 
 
+def get_icon():
+    # get images by url
+    url = f"http://openweathermap.org/img/w/{get_weather('Lutsk').json()['weather'][0]['icon']}.png"
+    response = requests.get(url)
+    icon_data = response.content
+    img = Image.open(BytesIO(icon_data))
+    img = img.resize((150, 150), Image.LANCZOS)
+    return img
+
+
+def get_temperature():
+    # "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API}"
+    return get_weather('Lutsk').json()['main']['temp']
+
+
+def get_description():
+    return get_weather('Lutsk').json()['weather'][0]['description']
+
+
+def more_details():
+    return get_weather('Lutsk').json()
+
+
 def display():
     window = tk.Tk()
     window.title(f"Weather in {get_weather('Lutsk')}")
     window.geometry("400x400")
-    # get images by url
-    url = f"http://openweathermap.org/img/w/{get_weather('Lutsk').json()['weather'][0]['icon']}.png"
-    response = requests.get(url)
-    data_content = response.content
-    img = Image.open(BytesIO(data_content))
-    img = ImageTk.PhotoImage(img)
-    icon = tk.Label(window, image=img)
-    # TODO: need to get a temperature from openweather by url
-    temperature = tk.Label(window, text=f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API}")
-    info = tk.Label(window, text="Info")
-    icon.grid(row=0, column=0)
+    # insert data from functions on main display
+    img = ImageTk.PhotoImage(get_icon())
+    icons = tk.Label(window, image=img, height=200, width=200)
+    temperature = tk.Label(window, text=get_temperature())
+    description = tk.Label(window, text=get_description())
+    details = tk.Label(window, text=more_details(), wraplength=400)
+    # use grid to structure data
+    icons.grid(row=0, column=0)
     temperature.grid(row=0, column=1)
-    # info.grid(row=1, column=0, columnspan=2)
+    description.grid(row=1, column=1)
+    details.grid(row=2, column=0, columnspan=2)
+
     window.mainloop()
 
 
 if __name__ == "__main__":
+    get_icon()
     display()
